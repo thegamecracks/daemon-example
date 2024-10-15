068d5d3e3f4af29059a72bf96d754815cdd4fcb2
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-10-15 15:23:06 -0400

    feat: set connect() timeout on windows
    At least on Windows 11, it takes a couple seconds for
    ConnectionRefusedError to be raised when the daemon is offline.

--- src/thedaemon/client.py
+++ src/thedaemon/client.py
@@ -7,13 +7,21 @@ from .constants import ADDRESS, PORT
 def connect_to_server() -> socket.socket:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    if sys.platform == "win32":
+        # Windows might take a bit before refusing a connection.
+        # Let's add a timeout to speed things along.
+        client.settimeout(0.1)
+
     try:
         client.connect((ADDRESS, PORT))
-    except ConnectionRefusedError:
+    except (ConnectionRefusedError, TimeoutError):
         client.close()
         sys.exit("Failed to connect to daemon; did you start it?")
     except BaseException:
         client.close()
         raise
+
+    if sys.platform == "win32":
+        client.settimeout(None)
 
     return client
